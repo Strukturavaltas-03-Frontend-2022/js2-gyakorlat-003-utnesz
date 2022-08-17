82cc0cc14aa864e6e5c6ea9e114f85759b9b46ae
utnesz <90829509+utnesz@users.noreply.github.com>
2022-08-17 12:33:45 +0200

    Done.

--- solutions/app2.js
+++ solutions/app2.js
@@ -1,4 +1,14 @@
+
 const user = {
   firstName: 'John',
   lastName: 'Doe',
+};
+
+
+const {lastName: l, firstName: f, j='unknown'} = user;
+
+export {
+  l,
+  f,
+  j,
 };
--- solutions/app3.js
+++ solutions/app3.js
@@ -1,2 +1,20 @@
+//## 3. feladat
+//    - A`solutions/app3.js` fájlban dolgozz!
+//        - Mentsd el külön`a`, `b` és `c` nevű változókba az alábbi tömb első, harmadik és ötödik indexű elemét!
+//            ```javascript 
+//    const names = ['John', 'Jane', 'Judith', 'Jennifer', 'Jeremiah', 'Johnny'];
+//
+
+
 const names = ['John', 'Jane', 'Judith', 'Jennifer', 'Jeremiah', 'Johnny'];
 
+const a = (names[1]);
+const b = (names[3]);
+const c = (names[5]);
+
+export {
+    a,
+    b,
+    c
+};
+ 
